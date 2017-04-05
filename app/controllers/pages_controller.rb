309f495e858562ce04eaabbfc044141098de7414
Sonia <soniambrown14@gmail.com>
2017-04-05 22:33:41 +0000

    Finishing Task 6

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,18 @@ class PagesController < ApplicationController
   end
 
   def sms
-    @message = params[:message]
+    @textmessage = params[:message]
     @phone = params[:usrtel]
+    account_sid = 'ACd333927084fe652886c7d2e981076d70' 
+    auth_token = '5d2a1364df47f337fa10eee629c98ec4' 
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    msg = @client.account.messages.create(
+      from: '+14133010510',
+      to: @phone,
+      body: @textmessage
+    )
+
+  puts msg.sid
+
   end
 end
